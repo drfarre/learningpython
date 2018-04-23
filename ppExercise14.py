827e9d521d624272e9cd11a6fa35d59359d753c8
drfarre <38358057+drfarre@users.noreply.github.com>
2018-04-23 15:07:57 -0300

    Create ppExercise14.py

---
+++ ppExercise14.py
@@ -0,0 +1,11 @@
+a = [0,1,4,4,6,8,8,9,12,14,15,15,18]
+
+#El comando SET lista el contenido de una lista, omitiendo los repetidos por defecto
+
+def unico():
+    print(set(a))
+    return
+
+
+
+unico()
